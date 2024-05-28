2cb2b37d7ad25715432bf3cd5b65c24fd44aaa07
Henrique Francisco Venzon <143184156+Henrique-Venzon@users.noreply.github.com>
2024-05-28 12:20:46 -0300

    atualizações

---
+++ js/dropTop.js
@@ -0,0 +1,26 @@
+document.getElementById('sub-menu-1').addEventListener('click', function() {
+    var submenu = document.getElementById('submenu-list-1');
+    if (submenu.style.display === 'block') {
+        submenu.style.display = 'none';
+    } else {
+        submenu.style.display = 'block';
+    }
+  });
+  document.getElementById('sub-menu-2').addEventListener('click', function() {
+    var submenu = document.getElementById('submenu-list-2');
+    if (submenu.style.display === 'block') {
+        submenu.style.display = 'none';
+    } else {
+        submenu.style.display = 'block';
+    }
+  });
+  
+  document.getElementById('sub-menu-3').addEventListener('click', function() {
+    var submenu = document.getElementById('submenu-list-3');
+    if (submenu.style.display === 'block') {
+        submenu.style.display = 'none';
+    } else {
+        submenu.style.display = 'block';
+    }
+  });
+  
