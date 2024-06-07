a09132683f296a729a47248285df3ff981ed892e
Iago Souza <iago.snd.souza@gmail.com>
2024-06-07 12:36:12 -0300

    atualizações

--- js/copiarcodigo.js
+++ js/copiarcodigo.js
@@ -1,16 +1,10 @@
-function copyText() {
-    var codeContent = document.querySelector(".code-content.active"); // Seleciona a div com a classe 'code-content' que está ativa
-    var range = document.createRange();
-    range.selectNode(codeContent);
-    window.getSelection().removeAllRanges();
-    window.getSelection().addRange(range);
-    document.execCommand("copy");
-    window.getSelection().removeAllRanges();
 
-    // Atualiza o valor do input para "Copiado"
-    var copiarButton = document.getElementById("copiar");
-    copiarButton.value = "Copiado";
-
-    // Altera o estilo de fundo para verde
-    copiarButton.style.backgroundColor = "lightgreen";
-}
+    function copyText() {
+        var codeContent = document.getElementById("code-content");
+        var range = document.createRange();
+        range.selectNode(codeContent);
+        window.getSelection().removeAllRanges();
+        window.getSelection().addRange(range);
+        document.execCommand("copy");
+        window.getSelection().removeAllRanges();
+    }
